16cfd8a6b95d998ff10ae3ee4902f7b237cbb99b
Jade Pappas <pappas.293@osu.edu>
2026-06-30 16:42:36 -0400

    improve UI

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const navLinks = document.querySelectorAll('.navbar__links, .button')
 
-// Display Mobile Menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 }
 
-menu.addEventListener('click', mobileMenu);
+menu.addEventListener('click', mobileMenu)
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('is-active')
+        menuLinks.classList.remove('active')
+    })
+})
